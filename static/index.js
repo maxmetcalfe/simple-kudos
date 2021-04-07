0c9994b8e68d3124167f6d0bb96b2856a1191cd1
Max Metcalfe <hello@maxmetcalfe.com>
2021-04-07 16:59:44 -0700

    Finish up initial logic

--- static/index.js
+++ static/index.js
@@ -1,28 +1,47 @@
 
 class SimpleKudos {
 
-  constructor(text, emoji, id, origin) {
-    this.text = text;
-    this.emoji = emoji;
-    this.id = id;
-    this.origin = origin;
+  constructor(options) {
+    this.text = options.text;
+    this.emoji = options.emoji;
+    this.color = options.color;
+    this.id = options.id;
+    this.elementId = options.elementId;
     this.serviceURL = "http://localhost:3000/kudos";
+    this.increment = 1;
 
-    this.element = document.getElementById("kudos");
+    // Styles
+    this.element = document.getElementById(this.elementId);
+    this.element.style.color = this.color;
+    this.element.style.cursor = "pointer";
+    this.element.style.borderRadius = "20px";
+    this.element.style.padding = "5px";
+    this.element.style.display = "inline-block";
+    this.element.style.display = "inline-block";
+
     if (!this.element) {
-      console.warn(`Make sure your page contains an element with id = "kudos".`)
+      console.warn(`Make sure your page contains an element with id equal to "${this.elementId}"!`)
     } else {
       this.element.addEventListener("click", this._update.bind(this));
       this._update();
     }
   }
 
-  _update() {
-    fetch(`${this.serviceURL}?id=${this.id}`)
+  _update(event) {
+    var url = `${this.serviceURL}?id=${this.id}`;
+    if (event) {
+      url = `${this.serviceURL}?id=${this.id}&add=${this.increment}`
+    } else {
+      url = `${this.serviceURL}?id=${this.id}`;
+    }
+    fetch(url)
       .then(response => response.json())
-      .then((count) => {
-        console.log(count);
-        this.element.innerHTML = this.text + count.count;
-      });
+      .then((data) => {
+        if (data.error) {
+          console.warn(data.error)
+          return;
+        }
+        this.element.innerHTML = this.emoji + data.count;
+      })
   }
 }
