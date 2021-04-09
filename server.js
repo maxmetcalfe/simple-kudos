038531375b15b259f9c21eb0303a8df77fdcd1e9
Max Metcalfe <hello@maxmetcalfe.com>
2021-04-08 18:28:12 -0700

    Remove unused server code

--- server.js
+++ server.js
@@ -1,37 +1,7 @@
 const express = require("express");
 const app = express();
 
-// Temp.
-let kudos = {};
-
 app.use(express.static("static"))
-
-app.get("/kudos", (req, res) => {
-  const host = req.get("host");
-  const id = req.query.id;
-
-  // Don't allow the use of curl.
-  if (req.headers["user-agent"].includes("curl")) {
-    res.status(403).json({ error: "hey there, chill" });
-    return;
-  }
-
-  if (Object.keys(kudos).indexOf(id) === -1) {
-    kudos[id] = 0;
-    // res.status(500).json({ error: "The provided simple-kudos ID does not exist." });
-    //return;
-  }
-
-  if (req.query.add) {
-    kudos[id] = kudos[id] + parseInt(req.query.add);
-  }
-
-  res.status(200).json({ count: kudos[id] });
-});
-
-app.get("/__debug", (req, res) => {
-  res.status(200).json(kudos);
-})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
