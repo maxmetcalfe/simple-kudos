9c5616aa2daa6e3bb4f6b60fe51ccac041aed473
Max Metcalfe <hello@maxmetcalfe.com>
2021-04-08 18:47:53 -0700

    Add logging

--- api/kudos.js
+++ api/kudos.js
@@ -35,11 +35,14 @@ module.exports = async (req, res) => {
   let toUpdate = existing.length ? existing[0] : null;
 
   if (!existing.length) {
+    console.log(`new entry: ${req.headers.referer}, ${id}, count: ${count}`);
     const initial = await kudos.insertOne({ referer: req.headers.referer, id: id, count: count });
   } else if (req.query.add) {
     count = toUpdate.count + req.query.add;
+    console.log(`update: ${req.headers.referer}, ${id}, count: ${count}`);
     const updated = await kudos.updateOne({ referer: req.headers.referer, id: id }, { $set: { count: count }});
   } else {
+    console.log(`fetch entry: ${req.headers.referer}, ${id}, count: ${count}`);
     count = toUpdate.count;
     const updated = await kudos.updateOne({ referer: req.headers.referer, id: id }, { $set: { count: count }});
   }
